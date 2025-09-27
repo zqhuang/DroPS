1ca9ab2b702efcdd744927fd7bf224a04ee5fdcc
Zhiqi Huang <huangzhq25@mail.sysu.edu.cn>
2025-09-27 11:25:48 +0800

    add requirements

--- mock_filtering.py
+++ mock_filtering.py
@@ -1,8 +1,16 @@
 from beforecmb import TOD_filtering
 import pickle
-nside = int(input("enter nside (128): \n"))
+nsidestr = input("enter nside (128): \n")
+if(nsidestr == ""):
+    nside = 128
+else:
+    nside = int(nsidestr)
+print("nside = " + str(nside))
 output = input("enter output filename (filter_128.pickle):\n")
-lmax = nside*3
+if(output == ""):
+    output = r"filter_" + str(nside) + r".pickle"
+print("output file: " + output)
+lmax = nside*3 - 1
 filtering = TOD_filtering(lmax = lmax, overall_factor = 1.,  lowl = 50., lowl_factor = 0.5, lowl_power = 4., l_mix = 0.02, m_mix = 0.02)
 with open(output, 'wb') as f:
     pickle.dump(filtering, f)
