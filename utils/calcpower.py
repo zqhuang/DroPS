8e1c54a11f435f349285770a57eee85747f5a703
Zhiqi Huang <huangzhq25@mail.sysu.edu.cn>
2025-11-27 17:11:42 +0800

    simplify README

--- utils/calcpower.py
+++ utils/calcpower.py
@@ -7,14 +7,14 @@ from sys import argv
 if(len(argv)<3):
     print("python calcpower.py mask map1 [map2]")
     exit()
-bpc = band_power_calculator(argv[1], like_fields=['BB'])
+bpc = band_power_calculator(argv[1], like_fields=['TT','TE',  'TB',  'EE', 'EB', 'BB'])
 if(len(argv)>3):
     bp = bpc.band_power(argv[2], argv[3])
 else:
     bp = bpc.band_power(argv[2])
 
-print(bp['BB'][0], end='')
-for i in range(1, bpc.num_ells):
-    print(' ', bp['BB'][i], end='')
-print("\n")
+#print(bp['BB'][0], end='')
+for i in range(bpc.num_ells):
+    print('band  #'+str(i), ", power spectra(TT,TE,TB,EE,EB,BB): ", bp['TT'][i],  bp['TE'][i], bp['TB'][i], bp['EE'][i], bp['EB'][i], bp['BB'][i])
+#print("\n")
     
